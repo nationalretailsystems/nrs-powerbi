730ea5f2b0d491fdc8856253da7d8bbf1c8ee9e4
mcorbo <mikecorbo@verizon.net>
2021-10-15 10:37:26 -0400

    New weight api

--- src/routes/api/powerbi/validators.ts
+++ src/routes/api/powerbi/validators.ts
@@ -3,5 +3,6 @@ import { InputCheckChain } from 'src/types';
 
 export const getViamundoWeight: InputCheckChain[] = [
     query('fromDate').exists().isNumeric().isLength({ min: 6, max: 6 }).withMessage('Must be date yyyymmdd'),
-    query('toDate').exists().isNumeric().isLength({ min: 6, max: 6 }).withMessage('Must be date yyyymmdd')
+    query('toDate').exists().isNumeric().isLength({ min: 6, max: 6 }).withMessage('Must be date yyyymmdd'),
+    query('building').isIn(['V','P']).exists().isAlpha().isUppercase().isLength({ min: 1, max: 1 }).withMessage('Building Must be 1 Character Uppercase Alpha')    
 ];

16bcd59ed6d8cbd1b50ee07806bdb73843946f86
mcorbo <mikecorbo@verizon.net>
2021-10-19 18:59:25 -0400

    add biuldings

--- src/routes/api/powerbi/validators.ts
+++ src/routes/api/powerbi/validators.ts
@@ -4,5 +4,5 @@ import { InputCheckChain } from 'src/types';
 export const getViamundoWeight: InputCheckChain[] = [
     query('fromDate').exists().isNumeric().isLength({ min: 6, max: 6 }).withMessage('Must be date yyyymmdd'),
     query('toDate').exists().isNumeric().isLength({ min: 6, max: 6 }).withMessage('Must be date yyyymmdd'),
-    query('building').isIn(['V','P']).exists().isAlpha().isUppercase().isLength({ min: 1, max: 1 }).withMessage('Building Must be 1 Character Uppercase Alpha')    
+    query('building').isIn(['V','P','O','H','N','W']).exists().isAlpha().isUppercase().isLength({ min: 1, max: 1 }).withMessage('Building Must be 1 Character Uppercase Alpha')    
 ];

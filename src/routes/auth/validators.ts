06628c17f16d9f05afb4a0f63b0f39b997f20733
mcorbo <mikecorbo@verizon.net>
2022-04-08 13:55:00 -0400

    nginx 4k

--- src/routes/auth/validators.ts
+++ src/routes/auth/validators.ts
@@ -1,9 +1,13 @@
-import { body } from 'express-validator';
+import { body, oneOf } from 'express-validator';
 import configService from 'config';
 import { InputCheckChain } from 'src/types';
 const regexes = configService.get().regexes;
 
 export const login: InputCheckChain[] = [
-    body('username').exists().isString(),
+    oneOf([body('username').exists().isString(),
     body('password').exists().isString().matches(regexes.password)
-];
+],[
+    body('client_id').exists().isString(),
+    body('client_secret').exists().isString().matches(regexes.password)
+]
+)];

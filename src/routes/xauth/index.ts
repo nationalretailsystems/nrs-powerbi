cdb2fac7b7980e420dd263962389e6ed19a0e013
mcorbo <mikecorbo@verizon.net>
2022-04-19 17:52:37 -0400

    fk token change

--- src/routes/xauth/index.ts
+++ src/routes/xauth/index.ts
@@ -3,7 +3,8 @@ import validate from 'src/middlewares/validate';
 import respond from 'src/middlewares/respond';
 import requireAuth from 'src/middlewares/require-auth';
 import * as validators from './validators';
-import * as user from 'src/controllers/user';
+import * as user from 'src/controllers/fkuser';
+
 
 export default function mountAuth(router: Router) {
     router.post(

2e0beb579b2d25688c0bf49d4aa68e100dad5bd0
mcorbo <mikecorbo@verizon.net>
2022-05-24 12:20:26 -0400

    Add Cost Center to Sales Api

--- src/models/powerbi-template6.ts
+++ src/models/powerbi-template6.ts
@@ -5,13 +5,13 @@ import { JSONObject } from 'src/types';
 
 export default new eradaniConnect.run.Sql(
     `with sales as (
-        select acct#h,totalh, prodth from wsfile2.blhd
+        select acct#h,totalh, prodth, ORICCH, DSTCCH from wsfile2.blhd
          union all
-        select acct#h, totalh, prodth  from kyfile.blhd
+        select acct#h, totalh, prodth, ORICCH, DSTCCH  from kyfile.blhd
          union all
-        select acct#h, totalh, prodth from wlfile.blhd
+        select acct#h, totalh, prodth, ORICCH, DSTCCH from wlfile.blhd
          union all
-        select acct#h, totalh, prodth  from mitfile.blhd
+        select acct#h, totalh, prodth, ORICCH, DSTCCH  from mitfile.blhd
         )
        SELECT
         CASE WHEN ORIGNA > ' ' THEN ORIGNA
@@ -19,7 +19,8 @@ export default new eradaniConnect.run.Sql(
         CASE WHEN ORIGNA > ' ' THEN DESCR1
          ELSE NAMEAA END AS ACCOUNT_NAME,
        SUM(totalh) AS "SumTotalh",
-       month(prodth) as "MONTH", year(prodth) as "YEAR"
+       month(prodth) as "MONTH", year(prodth) as "YEAR", 
+       ORICCH as ORIGCC, DSTCCH AS DESTCC
        From blpgm.cust
          LEFT OUTER JOIN blpgm.grup
            ON origna = corpg1 AND group1 = ' '
@@ -29,8 +30,8 @@ export default new eradaniConnect.run.Sql(
          CASE WHEN ORIGNA > ' ' THEN ORIGNA
             ELSE CAST(acct#a AS CHAR(5)) END,
          CASE WHEN ORIGNA > ' ' THEN DESCR1 ELSE NAMEAA END,
-         month(prodth), year(prodth)
-        ORDER BY 5,4`,
+         month(prodth), year(prodth), ORICCH, DSTCCH
+        ORDER BY 5,4,1,6,7`,
     {
         params: [
             {

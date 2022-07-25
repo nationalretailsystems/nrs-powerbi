dedc862a28cd20a65d51b67693f689f0ceffea77
mcorbo <mikecorbo@verizon.net>
2022-07-25 10:27:00 -0400

    keycap pro wo so api

--- src/controllers/keycap.ts
+++ src/controllers/keycap.ts
@@ -1,5 +1,6 @@
 import createLogger from 'src/services/logger';
 import SQLTemplateKCPROWSO, { SQLTemplateOutputKCPROWSO } from 'src/models/keycap-kc-prowso';
+import SQLTemplateKCPROWOSO, { SQLTemplateOutputKCPROWOSO } from 'src/models/keycap-kc-prowoso';
 // import { JSONObject } from 'src/types';
 import { powerbiTransports } from 'src/services/connection';
 // import { DateTime } from 'luxon';
@@ -13,4 +14,8 @@ const logger = createLogger('controllers/keycap');
 export async function getKCProWSO(): Promise<SQLTemplateOutputKCPROWSO> {
     logger.debug('Calling SQLTemplate program');
     return powerbiTransports.wolf.execute(SQLTemplateKCPROWSO) as Promise<SQLTemplateOutputKCPROWSO>;
+}
+export async function getKCProWoSO(): Promise<SQLTemplateOutputKCPROWOSO> {
+    logger.debug('Calling SQLTemplate program');
+    return powerbiTransports.wolf.execute(SQLTemplateKCPROWOSO) as Promise<SQLTemplateOutputKCPROWOSO>;
 }

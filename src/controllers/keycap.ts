761ed9642a20b13d3ada861a585369aa792645f1
mcorbo <mikecorbo@verizon.net>
2022-07-26 08:15:26 -0400

    kc dup invoice apis

--- src/controllers/keycap.ts
+++ src/controllers/keycap.ts
@@ -1,6 +1,8 @@
 import createLogger from 'src/services/logger';
 import SQLTemplateKCPROWSO, { SQLTemplateOutputKCPROWSO } from 'src/models/keycap-kc-prowso';
 import SQLTemplateKCPROWOSO, { SQLTemplateOutputKCPROWOSO } from 'src/models/keycap-kc-prowoso';
+import SQLTemplateKCDUPINVPRO, { SQLTemplateOutputKCDUPINVPRO } from 'src/models/keycap-kc-dupinvpro';
+import SQLTemplateKCDUPINVSCAC, { SQLTemplateOutputKCDUPINVSCAC } from 'src/models/keycap-kc-dupinvscac';
 // import { JSONObject } from 'src/types';
 import { powerbiTransports } from 'src/services/connection';
 // import { DateTime } from 'luxon';
@@ -18,4 +20,12 @@ export async function getKCProWSO(): Promise<SQLTemplateOutputKCPROWSO> {
 export async function getKCProWoSO(): Promise<SQLTemplateOutputKCPROWOSO> {
     logger.debug('Calling SQLTemplate program');
     return powerbiTransports.wolf.execute(SQLTemplateKCPROWOSO) as Promise<SQLTemplateOutputKCPROWOSO>;
+}
+export async function getKCDupInvPro(): Promise<SQLTemplateOutputKCDUPINVPRO> {
+    logger.debug('Calling SQLTemplate program');
+    return powerbiTransports.wolf.execute(SQLTemplateKCDUPINVPRO) as Promise<SQLTemplateOutputKCDUPINVPRO>;
+}
+export async function getKCDupInvScac(): Promise<SQLTemplateOutputKCDUPINVSCAC> {
+    logger.debug('Calling SQLTemplate program');
+    return powerbiTransports.wolf.execute(SQLTemplateKCDUPINVSCAC) as Promise<SQLTemplateOutputKCDUPINVSCAC>;
 }

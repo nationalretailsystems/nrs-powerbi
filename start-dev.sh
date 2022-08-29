308bd6443baa0d746b644fca16cabcc83545e6fd
Aaron Magid <aaron@eradani.com>
2022-08-29 09:09:06 -0500

    Split start.sh into dev/test/prod scripts

---
+++ start-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+NODE_HOME=/QOpenSys/pkgs/bin
+ECNCT_HOME=/QOpenSys/opt/eradani/dev/eradani-inout-template
+
+NODE_ENV=development
+export NODE_ENV
+
+PATH=$NODE_HOME:$PATH
+
+cd $ECNCT_HOME
+npm start
--- start-test.sh
+++ start-test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 NODE_HOME=/QOpenSys/pkgs/bin
-ECNCT_HOME=/QOpenSys/opt/eradani/eradani-inout-template
+ECNCT_HOME=/QOpenSys/opt/eradani/test/eradani-inout-template
 
-NODE_ENV=production
+NODE_ENV=test
 export NODE_ENV
 
 PATH=$NODE_HOME:$PATH

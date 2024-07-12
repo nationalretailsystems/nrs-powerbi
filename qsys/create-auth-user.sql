0118f8c93ed1a4d6555b9791a271bb4aff97570a
Aaron Magid <aaron@eradani.com>
2024-07-12 17:03:18 -0500

    Add default auth user without commitment control

--- qsys/create-auth-user.sql
+++ qsys/create-auth-user.sql
@@ -1,3 +1,4 @@
 INSERT INTO @LIBRARY@.USERS
     (clientId, clientSecret, scope)
-    VALUES ('eradani', '$2a$10$.XV0Go5VKuyL/FGy/DqL2.V/mG55BCNaUnkus0pV.fVyyrDLfaZTC', 'sample_scope');
+    VALUES ('eradani', '$2a$10$.XV0Go5VKuyL/FGy/DqL2.V/mG55BCNaUnkus0pV.fVyyrDLfaZTC', 'sample_scope')
+    WITH NONE;

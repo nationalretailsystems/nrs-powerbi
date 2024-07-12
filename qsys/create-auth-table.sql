e6d45a1b9a2346e9c5a9c885b9254d4dd85fe9e0
Aaron Magid <aaron@eradani.com>
2024-07-12 17:02:39 -0500

    Split auth table from auth user sql

--- qsys/create-auth-table.sql
+++ qsys/create-auth-table.sql
@@ -4,7 +4,3 @@ CREATE OR REPLACE TABLE @LIBRARY@.USERS (
     scope varchar(360),
     primary key (clientId)
 );
-
-INSERT INTO @LIBRARY@.USERS
-    (clientId, clientSecret, scope)
-    VALUES ('eradani', '$2a$10$.XV0Go5VKuyL/FGy/DqL2.V/mG55BCNaUnkus0pV.fVyyrDLfaZTC', 'sample_scope');
---
+++ qsys/create-auth-user.sql
@@ -0,0 +1,3 @@
+INSERT INTO @LIBRARY@.USERS
+    (clientId, clientSecret, scope)
+    VALUES ('eradani', '$2a$10$.XV0Go5VKuyL/FGy/DqL2.V/mG55BCNaUnkus0pV.fVyyrDLfaZTC', 'sample_scope');
